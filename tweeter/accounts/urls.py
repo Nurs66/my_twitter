1963735cd5a4f6d1a458be5575c87795454bbd75
PythonNurs <eldosnursultan@gmail.com>
2019-04-26 18:13:21 +0600

    added followiing

--- tweeter/accounts/urls.py
+++ tweeter/accounts/urls.py
@@ -1,9 +1,11 @@
 from .views import (
     UserDetailView,
+    UserFollowView,
 )
 from django.urls import path
 from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path('<username>/', UserDetailView.as_view(), name='detail'),
+    path('<username>/follow', UserFollowView.as_view(), name='follow'),
 ]
