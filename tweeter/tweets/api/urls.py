33d7ebdf8b5989cbc43231b2b67f7439a8244ff8
PythonNurs <eldosnursultna@gmail.com>
2019-05-07 16:19:52 +0600

    add like

--- tweeter/tweets/api/urls.py
+++ tweeter/tweets/api/urls.py
@@ -2,6 +2,7 @@ from .views import (
     RetweetAPIView,
     TweetListAPIView,
     TweetCreateAPIView,
+    LikeToggleAPIView,
 )
 
 from django.urls import path
@@ -11,5 +12,6 @@ urlpatterns = [
     # path('', RedirectView.as_view(url='/')),
     path('', TweetListAPIView.as_view(), name='list'),  # /api/tweet/
     path('create/', TweetCreateAPIView.as_view(), name='create'), # /api/tweet/create/
-    path('<int:pk>/retweet/', RetweetAPIView.as_view(), name='retweet'), #/api/tweet/id/retweet
+    path('<int:pk>/like/', LikeToggleAPIView.as_view(), name='like-toggle'),
+    path('<int:pk>/retweet/', RetweetAPIView.as_view(), name='retweet'),#/api/tweet/id/retweet
 ]
